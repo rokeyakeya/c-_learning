a2fedd62b2d6183d59454663abb854f37c7bf64a
rokeyakeya <rokeya_keya@yahoo.com>
2015-09-28 11:20:21 -0700

    rokeya keya 1st commit

---
+++ first_csharp_rokeya_keya/first_csharp_rokeya_keya/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_csharp_rokeya_keya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("hello world ");
+        }
+    }
+}
